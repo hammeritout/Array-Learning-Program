59fcab45660cc53e55a06f3d0d82dda0f51f7888
Sharyl D. Hammer <sharyl1266@gmail.com>
2018-09-23 13:46:07 -0500

    array program

---
+++ Module8ArrayProject/frmOfLight.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Module8ArrayProject
+{
+    partial class frmOfLight
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtNumberEntered = new System.Windows.Forms.TextBox();
+            this.lblNumberEntered = new System.Windows.Forms.Label();
+            this.btnAddArray = new System.Windows.Forms.Button();
+            this.btnShowArray = new System.Windows.Forms.Button();
+            this.lblArrayDisplay = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnResetArray = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(414, 171);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(111, 35);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtNumberEntered
+            // 
+            this.txtNumberEntered.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumberEntered.Location = new System.Drawing.Point(178, 59);
+            this.txtNumberEntered.Name = "txtNumberEntered";
+            this.txtNumberEntered.Size = new System.Drawing.Size(349, 34);
+            this.txtNumberEntered.TabIndex = 2;
+            // 
+            // lblNumberEntered
+            // 
+            this.lblNumberEntered.AutoSize = true;
+            this.lblNumberEntered.Location = new System.Drawing.Point(45, 70);
+            this.lblNumberEntered.Name = "lblNumberEntered";
+            this.lblNumberEntered.Size = new System.Drawing.Size(100, 17);
+            this.lblNumberEntered.TabIndex = 3;
+            this.lblNumberEntered.Text = "Enter Number:";
+            // 
+            // btnAddArray
+            // 
+            this.btnAddArray.Location = new System.Drawing.Point(21, 171);
+            this.btnAddArray.Name = "btnAddArray";
+            this.btnAddArray.Size = new System.Drawing.Size(111, 35);
+            this.btnAddArray.TabIndex = 4;
+            this.btnAddArray.Text = "Add Array";
+            this.btnAddArray.UseVisualStyleBackColor = true;
+            this.btnAddArray.Click += new System.EventHandler(this.btnAddArray_Click);
+            // 
+            // btnShowArray
+            // 
+            this.btnShowArray.Location = new System.Drawing.Point(152, 171);
+            this.btnShowArray.Name = "btnShowArray";
+            this.btnShowArray.Size = new System.Drawing.Size(111, 35);
+            this.btnShowArray.TabIndex = 5;
+            this.btnShowArray.Text = "Show Array";
+            this.btnShowArray.UseVisualStyleBackColor = true;
+            this.btnShowArray.Click += new System.EventHandler(this.btnShowArray_Click);
+            // 
+            // lblArrayDisplay
+            // 
+            this.lblArrayDisplay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblArrayDisplay.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArrayDisplay.Location = new System.Drawing.Point(738, 28);
+            this.lblArrayDisplay.Name = "lblArrayDisplay";
+            this.lblArrayDisplay.Size = new System.Drawing.Size(198, 417);
+            this.lblArrayDisplay.TabIndex = 6;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lblMessage.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(21, 279);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(698, 166);
+            this.lblMessage.TabIndex = 7;
+            // 
+            // btnResetArray
+            // 
+            this.btnResetArray.Location = new System.Drawing.Point(283, 171);
+            this.btnResetArray.Name = "btnResetArray";
+            this.btnResetArray.Size = new System.Drawing.Size(111, 35);
+            this.btnResetArray.TabIndex = 1;
+            this.btnResetArray.Text = "Reset Array";
+            this.btnResetArray.UseVisualStyleBackColor = true;
+            this.btnResetArray.Click += new System.EventHandler(this.btnResetArray_Click);
+            // 
+            // frmOfLight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(992, 468);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblArrayDisplay);
+            this.Controls.Add(this.btnShowArray);
+            this.Controls.Add(this.btnAddArray);
+            this.Controls.Add(this.lblNumberEntered);
+            this.Controls.Add(this.txtNumberEntered);
+            this.Controls.Add(this.btnResetArray);
+            this.Controls.Add(this.btnExit);
+            this.Name = "frmOfLight";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Show me the Numbers";
+            this.Load += new System.EventHandler(this.frmOfLight_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtNumberEntered;
+        private System.Windows.Forms.Label lblNumberEntered;
+        private System.Windows.Forms.Button btnAddArray;
+        private System.Windows.Forms.Button btnShowArray;
+        private System.Windows.Forms.Label lblArrayDisplay;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnResetArray;
+    }
+}
+
---
+++ Module8ArrayProject/frmOfLight.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module8ArrayProject
+{
+    public partial class frmOfLight : Form
+    {
+        // Class Array
+        long[] NumberArray;
+        const int MAXARRAYSIZE = 13;
+        int index = 0;
+        long[] totals = new long[13];
+        
+
+               
+        public frmOfLight()
+        {
+            InitializeComponent();
+        }
+
+       private void Clear_Array(long []NumberArray, long ClearValue)
+        {
+            for (int lcv = 0; lcv < MAXARRAYSIZE; lcv++)
+            {
+                NumberArray[lcv] = ClearValue;
+
+            }
+
+
+        }
+
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAddArray_Click(object sender, EventArgs e)
+        {
+
+            lblMessage.Text = "";
+            long lNumber = -1;
+            
+            bool Add_To_Array = true; 
+
+            if(NumberArray.Length > 0)
+            {
+
+                if(NumberArray[NumberArray.Length -1] > 0)
+
+                    //Array if Full
+                    // -1 to find when the array is full. Subtracting from the end of the Array.
+                {
+                    lblMessage.Text = "The Array is full. Please do not try to add more numbers to Array.";
+                    Add_To_Array = false; 
+                }
+
+
+            }
+             
+                         
+                   if (txtNumberEntered.Text == "")
+                     {
+                // Text number not entered
+                   lblMessage.Text = "You need to enter a number in the Number Entered textbox.";
+                   Add_To_Array = false; 
+                    }
+                    else
+                    {
+
+                    try
+                   {
+                    // Validation for Number Ranges
+                    lNumber = Convert.ToInt64(txtNumberEntered.Text);
+                    decimal lnumber = 0m;
+                    if (Decimal.TryParse(txtNumberEntered.Text, out lnumber))
+                   {
+                   ///return true;
+                    }
+                    else
+                     {
+                     lblMessage.Text = lblMessage.Text + "Number Entered" + " must be a whole number value." + "\n\n";
+                   Add_To_Array = false;
+                    }
+
+                    
+                    long number = Convert.ToInt64(txtNumberEntered.Text);
+                   long min = -1000;
+                   long max = 1000;
+
+                if (number < min || number > max)
+
+                  {
+                    lblMessage.Text = ("Invalid range " + "Number Entered" + " must be between " + "-1,000" + " and " + "1,000" + ".");
+                    txtNumberEntered.Focus();
+                     Add_To_Array = false;
+                 }
+                
+
+                 }
+
+
+                 catch(Exception ex)
+                   {
+                     lblMessage.Text =ex.Message;  
+                 }
+                              
+
+            }
+
+            
+
+               while(Add_To_Array)
+                 {
+                if (index >= NumberArray.Length)
+                {
+                    Add_To_Array = false;
+                }
+                else
+                {
+                    if (NumberArray[index] <= 0)
+                    {
+                        (NumberArray[index]) = lNumber;
+                        Add_To_Array = false;
+
+                    }
+
+                }
+
+                index++;
+
+              }
+
+        }
+
+
+        private void frmOfLight_Load(object sender, EventArgs e)
+        {
+            //Creates an instance of the Array.
+            NumberArray = new long[MAXARRAYSIZE];
+
+            Clear_Array(NumberArray, -1);
+        }
+
+        private void btnShowArray_Click(object sender, EventArgs e)
+        {
+
+
+            bool Add_To_Array = false;
+            int index = 0;
+            long TotalofNumbers = 0;
+            long NumberCount = 0;
+
+
+            decimal Average = 0.0m;
+
+            string NumberArrayContents = "";
+
+            txtNumberEntered.Text = "";
+            if (NumberArray.Length > 0)
+            {
+                if (NumberArray[0] > 0)
+                {
+                    Add_To_Array = true;
+                }
+                else
+                {
+                    NumberArrayContents = "The Array is empty.";
+
+                }
+            }
+
+            while (Add_To_Array)
+            {
+                totals[index] += NumberArray[index];
+                NumberArrayContents += (NumberArray[index].ToString() + "\n");
+                
+                TotalofNumbers += NumberArray[index];
+               
+
+                NumberCount++;
+                index += 1;
+
+                if (index >= NumberArray.Length)
+                {
+                    Add_To_Array = false;
+
+                }
+
+                else
+                {
+
+                    if (NumberArray[index] <= 0)
+                    {
+
+
+
+                        Add_To_Array = false;
+
+                    }
+
+
+
+                }
+
+            }
+
+             
+                   
+               
+          //    Array.Sort(array);
+            string totalString ="";
+            decimal highvalue =0;
+            decimal lowvalue = 999;
+            Array.Sort(totals);
+            foreach (decimal total in totals)
+            {
+               
+
+                if (total > 0)
+                {
+                    highvalue = 0;
+                    if (total > highvalue)
+                    {
+                        highvalue = total;
+                    }
+
+                                       
+                }
+                
+            }
+
+         //   Array.Sort(totals);
+               foreach (decimal total in totals)
+            
+                {
+                    if (total > 0)
+                {
+                    
+                    if (total < lowvalue)
+                    {
+                        lowvalue = total;
+                    }
+                    
+                }
+                
+            }
+
+              
+                  
+          lblArrayDisplay.Text =  NumberArrayContents;
+            
+            lblArrayDisplay.Update();
+
+            Average = (decimal)TotalofNumbers / (decimal)NumberCount;
+            Average = Math.Round(Average, 3);
+            
+            lblMessage.Text = "The average of the number in the array is > " + Average.ToString() + "\n"
+             + "The total of numbers in the array " + NumberCount + "\n"
+             + "The lowest value in the array " + lowvalue + "\n" 
+            + "The highest value in the array " + highvalue; 
+
+
+
+        }
+
+       
+        private void btnResetArray_Click(object sender, EventArgs e)
+        {
+            Clear_Array(NumberArray, -1);
+            Clear_Array(totals, 0);
+            index = 0;
+             
+            lblArrayDisplay.Text = "";
+            lblMessage.Text = "The Array has been cleared.";
+
+        }
+    }
+}
